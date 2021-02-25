7694262aa7704c8699cd857f4e2df12a4d83eccf
danielxts555@gmail.com <danielxts555@gmail.com>
2021-02-24 21:16:26 -0300

    Testes iniciais das peças com movimentos possiveis das peças

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace tabuleiro
 {
-    class Peca
+   abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -18,9 +18,12 @@ namespace tabuleiro
             this.cor = cor;
             this.qteMovimentos = 0;
         }
-         public void incrementarQteMovimento()
+         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
     }
 }
--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -23,7 +23,7 @@ namespace xadrez
         public void executaMovimento(Posicao origem, Posicao destino)
         {
             Peca p = tab.retirarPeca(origem);
-            p.incrementarQteMovimento();
+            p.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
